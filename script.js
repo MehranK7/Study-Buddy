1738dc3b0c7b69c6c5fd6a60f6f83306ff2466b6
MehranK7 <87976749+MehranK7@users.noreply.github.com>
2025-02-06 13:11:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Play Video
+function playVideo() {
+    alert("Play the explanation video.");
+  }
+  
+  // Dark Mode Toggle
+  document.getElementById('dark-mode-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
